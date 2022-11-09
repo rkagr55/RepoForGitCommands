929883b4afa0f4632f97476f9d152ff5d14a8876
rkagr55 <rkagr55@gmail.com>
2022-11-09 18:42:22 +0530

    Revert "reverting commit"
    tttt reverts commit f68237382794370b6776ea8b08fa3c48cde6c7a5.

--- GitHubPractice/src/A.java
+++ GitHubPractice/src/A.java
@@ -1,6 +1,6 @@
 public class A {
 
 	public static void main(String[] args) {
-		System.out.println("One Two Three Four");
+		System.out.println("Hello world program update agian again again");
 	}
 }
