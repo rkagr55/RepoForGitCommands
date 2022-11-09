30879d4e37028767e73473836b81b20e8aac9456
rkagr55 <rkagr55@gmail.com>
2022-11-09 16:36:25 +0530

    Revert "new commit 3"
    This reverts commit 94169ce054d00e130e9783637f97067628597433.

--- GitHubPractice/src/A.java
+++ GitHubPractice/src/A.java
@@ -1,6 +1,6 @@
 public class A {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world program updated again");
+		System.out.println("Hello world program updated");
 	}
 }
