4da4a7d3252ca21f863f6c76dc9b684fe5cc9144
rkagr55 <rkagr55@gmail.com>
2022-11-11 21:25:51 +0530

    Revert "reverting test"
    This reverts commit 8f12a318b81919977b84df3324a84099e8fe372a.

--- GitHubPractice/src/A.java
+++ GitHubPractice/src/A.java
@@ -1,9 +1,6 @@
-
 public class A {
 
 	public static void main(String[] args) {
-		System.out.println("class created");
-
+		System.out.println("Hello world program update agian again again");
 	}
-
 }
