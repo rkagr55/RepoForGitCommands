0728137b73d6cf48666651c6bbcbcc889f2efdec
rkagr55 <rkagr55@gmail.com>
2022-11-18 22:03:31 +0530

    messageing

--- GitHubPractice/src/A.java
+++ GitHubPractice/src/A.java
@@ -1,10 +1,6 @@
 public class A {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("Commit 3");
-=======
-		System.out.println("Hello chotu");
->>>>>>> parent of 3212f32 (commiting 1)
 	}
 }
