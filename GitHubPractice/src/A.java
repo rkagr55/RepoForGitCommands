15da3d35a0a1b2fec3e8f12a33309ffc433dbea5
rkagr55 <rkagr55@gmail.com>
2022-11-18 21:45:40 +0530

    Revert "commiting 1"
    This reverts commit 3212f32c5962b20767b44e50bddbe455a478bece.

--- GitHubPractice/src/A.java
+++ GitHubPractice/src/A.java
@@ -1,6 +1,10 @@
 public class A {
 
 	public static void main(String[] args) {
+<<<<<<< HEAD
 		System.out.println("Commit 3");
+=======
+		System.out.println("Hello chotu");
+>>>>>>> parent of 3212f32 (commiting 1)
 	}
 }
