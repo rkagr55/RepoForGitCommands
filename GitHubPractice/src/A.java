b4dea275731e6be3085a5dda63b80cc823308200
rkagr55 <rkagr55@gmail.com>
2022-12-05 22:07:28 +0530

    -mmm

--- GitHubPractice/src/A.java
+++ GitHubPractice/src/A.java
@@ -2,9 +2,7 @@ public class A {
 
 	public static void main(String[] args) {
 		System.out.println("Commit 003");
-		
 		System.out.println("new line");
-		
-				System.out.println("new line 1");
+		System.out.println("new line 1");
 	}
 }
